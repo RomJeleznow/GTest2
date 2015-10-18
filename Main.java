842585cad5fd2b57ee8db847153136a934de7009
RomJeleznow <rom.jeleznow@yandex.ru>
2015-10-18 23:21:08 +0300

    Add new class IntegerGenerator

--- Main.java
+++ Main.java
@@ -1,9 +1,11 @@
 
-
 class Main{
 	public static void main(String [] args){
 		System.out.println("Start main func.");
 		System.out.println(new Main());
+		IntegerGenerator ig=  new IntegerGenerator();
+		for(int i =0 ; i < 5; i++)
+			System.out.println(ig.next());
 	}
 	
 	public String toString(){
