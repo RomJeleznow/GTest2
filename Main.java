77e730949fc4c7219bd4a855af80cbafd59d3ea8
RomJeleznow <rom.jeleznow@yandex.ru>
2015-10-18 22:46:24 +0300

    Add toString func

--- Main.java
+++ Main.java
@@ -3,6 +3,10 @@
 class Main{
 	public static void main(String [] args){
 		System.out.println("Start main func.");
-		
+		System.out.println(new Main());
+	}
+	
+	public String toString(){
+		return "The main class.";
 	}
 }
